e8230851c9d7cb2cae0f4d429c420481147c06a1
mari2diaz <135451566+mari2diaz@users.noreply.github.com>
2023-06-22 21:04:03 -0400

    Update Servidor.cpp

--- Servidor.cpp
+++ Servidor.cpp
@@ -1,97 +1,65 @@
 #include "sockets.hpp"
 
-std::vector<SOCKET> clientes;
-
-void ProcesarCliente(SOCKET clienteSocket){
+void RecibirMensajes(SOCKET clienteSocket){
 	char buffer[BUFFER_SIZE];
 	while(true){
 		memset(buffer, 0, BUFFER_SIZE);
-		// Recibir mensaje del cliente
+		// Recibir mensaje del servidor
 		int leerBytes = recv(clienteSocket, buffer, BUFFER_SIZE, 0);
-		if(leerBytes == 0){
-			// Error o cliente desconectado
-			continue;
-		}
-		
-		// Verificar si es comando de salida
-		if(strncmp(buffer, "salir", 5) == 0){
-			std::cout << "Cliente " << clienteSocket << " desconectado." << std::endl;
+		if(leerBytes <= 0){
+			// Error o servidor desconectado
 			break;
 		}
-		
-		// Envia mensaje a todos los demas clientes conectados
-		for(SOCKET cliente : clientes){
-			if(cliente != clienteSocket){
-				send(cliente, buffer, leerBytes, 0);
-			}
-		}
-	}
-	
-	// Cerrar todos los sockets de los clientes
-	for(SOCKET cliente : clientes){
-		closesocket(clienteSocket);
-	}
-	clientes.clear();
 
-	std::cout << "Todos los clientes han sido desconectados. Cerrando el servidor." << std::endl;
-	exit(0);
+		std::string mensaje(buffer);
+        std::cout << "Mensaje recibido: " << mensaje << std::endl;
+	}
 }
 
 int main(){
-	
+
 	if(!InicializarSockets()){
 		std::cerr << "Error al inicializar Winsock." << std::endl;
 		return 1;
 	}
-	
-	// Crear socket del servidor
-	SOCKET servidorSocket = CrearSocket();
+
+	// Crear el socket del cliente
+	SOCKET clienteSocket = CrearSocket();
+
+	// Obtener la direccion IP del servidor
+	std::string direccionIP = ObtenerDireccionIPServidor("localhost", PUERTO);
+	if(direccionIP.empty()){
+		std::cerr << "No se pudo obtener la direccion IP del servidor." << std::endl;
+		CerrarSockets(clienteSocket);
+		return 1;
+	}
 	
 	// Configurar direccion del servidor
-	sockaddr_in servidorAddress;
+	sockaddr_in servidorAddress{};
 	servidorAddress.sin_family = AF_INET;
-	servidorAddress.sin_addr.s_addr = inet_addr("192.168.1.105");
-	servidorAddress.sin_port = htons(PUERTO); 
-	
-	std::cout << "Direccion IP del servidor: " << ObtenerDireccionIP(servidorSocket) << std::endl;
-	
-	// Enlazar socket a la direccion del servidor
-	if(bind(servidorSocket, reinterpret_cast<struct sockaddr*>(&servidorAddress), sizeof(servidorAddress)) == SOCKET_ERROR){
-		std::cerr << "Error al enlazar el socket." << std::endl;
+	servidorAddress.sin_addr.s_addr = inet_addr(direccionIP.c_str());
+	servidorAddress.sin_port = htons(PUERTO);
+
+	// Conectar al servidor
+	if(connect(clienteSocket, reinterpret_cast<struct sockaddr*>(&servidorAddress), sizeof(servidorAddress)) == SOCKET_ERROR){
+		std::cerr << "Error al conectar al servidor" << std::endl;
+		CerrarSockets(clienteSocket);
 		return 1;
 	}
-	
-	// Escuchar nuevas conexiones
-	if(listen(servidorSocket, MAX_CLIENTES) == SOCKET_ERROR){
-		std::cerr << "Error al escuchar nuevas conexiones." << std::endl;
-		return 1;
+
+	// Crear hilo para recibir mensajes
+	std::thread recibirThread(RecibirMensajes, clienteSocket);
+	recibirThread.detach();
+
+	std::string mensaje;
+	while(true){
+		std::getline(std::cin, mensaje);
+
+        EnviarMensaje(clienteSocket, mensaje.c_str());
 	}
-	
-	std::cout << "Servidor en ejecucion. Esperando clientes......" << std::endl;
-	
-    while(true){
-        // Aceptar nueva conexion de cliente
-        SOCKET clienteSocket = AceptarConexionCliente(servidorSocket);
 
-        std::cout << "Cliente " << clienteSocket << " conectado." << std::endl;
+	CerrarSockets(clienteSocket);
 
-        // Enviar mensaje al cliente
-        const char* mensaje = "¡Bienvenido! Al simulacro de WhatsApp.\n";
-        send(clienteSocket, mensaje, strlen(mensaje), 0);
-
-        // Agregar el nuevo cliente a la lista
-        clientes.push_back(clienteSocket);
-
-        if(clientes.size() == MAX_CLIENTES){
-            std::cout << "Se han alcanzado el numero maximo de clientes." << std::endl;
-        }
-
-        // Crear un hilo para procesar al cliente
-        std::thread clienteThread(ProcesarCliente, clienteSocket);
-        clienteThread.detach();
-    }
-	
-	CerrarSockets(servidorSocket);
-	
 	return 0;
 }
+
